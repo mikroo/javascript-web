776e823162e307ee85bdb04abdaf741fc3766c97
ch.ntimba <ch.ntimba@bluewin.ch>
2016-12-18 22:07:42 +0100

    finition du cours chapitre 5

--- chapitre_5/js/cours.js
+++ chapitre_5/js/cours.js
@@ -15,7 +15,7 @@ document.getElementById('bouton').addEventListener('click', function(e){
 });
 
 */
-
+/*
 // Appui sur une touche du clavier
 
 // Gestion de l'appui sur une touche du clavier produisant un caractère
@@ -55,5 +55,56 @@ function getBoutonSouris(code) {
 
 // Affiche des informations sur un événement souris
 function infosSouris(e) {
-	console.log("Evènement souris : " + e.type + ", bouon " + getBoutonSouris(e.button))
+	console.log("Evènement souris : " + e.type + ", bouon " + getBoutonSouris(e.button) + ", X : " + e.clientX + ", Y : " + e.clientY);
 }
+
+// Gestion du clic souris
+document.addEventListener('click', infosSouris);
+
+// Gestion de l'appui et du relâchement d'un bouton de la souris
+document.addEventListener("mousedown", infosSouris);
+document.addEventListener('mouseup', infosSouris);
+
+// Fin du chargement de la page web
+
+// Gestion de la fin du chargmenet de la page web
+window.addEventListener('load', function() {
+	console.log("Page entièrement chargée");
+});
+
+
+// Gestion de la fermeture de la page web
+window.addEventListener("beforeunload", function(e) {
+	var message = "On est bien ici !";
+	e.returnValue = message // Provoque une demande de confirmation (standard)
+	return message; // Provoque une demande de confirmation (certains navigateurs)
+});
+*/
+// Gestion du clic sur le document
+document.addEventListener('click', function() {
+	console.log("Gestionnaire document");
+});
+
+// Gestion du clic sur le paragraphe
+document.getElementById('para').addEventListener('click', function(){
+	console.log("Gestionnaire paragraphe");
+});
+
+// Gestion du clic sur le bouton
+document.getElementById('propa').addEventListener('click', function(e){
+	console.log("Gestionnaire bouton");
+})
+
+
+// Gestion du clic sur le bouton
+document.getElementById('propa').addEventListener('click', function(e) {
+	console.log("Gestionnaire bouton");
+	e.stopPropagation(); // Arrêt de la propagation de l'événenement
+});
+
+// Modifier le comportement par défaut en cas d'événement
+document.getElementById("interdit").addEventListener('click', function(e){
+	console.log("Contunuez plutôt à lire le cours ;)");
+	e.preventDefault(); // Annulation de la navigation vers la cible du lien
+});
+
