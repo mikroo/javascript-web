bc72343fe1e653974b0587e94dbd0d567b88a429
ch.ntimba <ch.ntimba@bluewin.ch>
2016-12-24 23:25:02 +0100

    exercice couleurs

--- chapitre_5/js/compteurClics.js
+++ chapitre_5/js/compteurClics.js
@@ -1,7 +1,7 @@
 /*
 Exercice : compter les clics
 */
-
+/*
 function clic() {
 	compteurClics++;
 	document.getElementById("compteurClics").textContent = compteurClics;
@@ -14,4 +14,17 @@ document.getElementById('clic').addEventListener('click', clic);
 
 document.getElementById('desactiver').addEventListener('click', function(){
 	document.getElementById('clic').removeEventListener('click', clic);
-})
+});
+
+*/
+function clic(){
+	compteurClics++; // incrémenté les nombre des clics
+	document.getElementById('compteurClics').textContent = compteurClics; // Afficher les nombre des clics
+}
+
+var compteurClics = 0; // Initiation de nombre des clics
+document.getElementById('clic').addEventListener('click', clic); // Evenement du bouton cliquez-mois
+
+document.getElementById('desactiver').addEventListener('click', function(){
+	document.getElementById('clic').removeEventListener('click', clic);
+});
