a964926fc1f7cc5b4053c1b8c85b1b75f714161e
Ch. Ntimba <ch.ntimba@bluewin.ch>
2016-12-28 23:39:27 +0100

    je gere liste d'affichage du tableau

--- chapitre_6/js/pays.js
+++ chapitre_6/js/pays.js
@@ -20,12 +20,38 @@ var listePays = [
     "Azerbaïdjan"
 ];
 
-listePays.forEach(function(pays){
-    // recupere id suggestions
-    var suggestionsElt = document.getElementById('suggestions');
-    suggestionsElt.textContent = pays;
-    // document.appendChild(suggestionsElt);
+// Gerer l'input
+var inputElt = document.getElementById('pays');
+inputElt.addEventListener('input', function(e){
+    // comparer l'element entrer par l'utilisateur si il correspond au tableau
+    // lister les pay
+    for(var i = 0; i < listePays.length; i++) {
+        // console.log(listePays[i]);
+        motRechercher = listePays[i].indexOf(e.target.value);
+        if(motRechercher !== -1) {
+            // console.log(motRechercher);
+            // console.log(listePays[i]);
+            // creer un element div
+            var divElt = document.createElement('div');
+            divElt.className = "suggestion";
+            divElt.textContent = listePays[i];
 
+            document.getElementById('suggestions').innerHTML = "";
+            document.getElementById('suggestions').appendChild(divElt); // Ajout des element dans la page
 
-
+            // gerer l'interaction dans pour pouvoir clicker l'élément et l'inserer dans l'input
+            divElt.addEventListener('click', function(e){
+                // recupere l'élément input pour pouvoir inserer la sugestions
+                document.getElementById('pays').value = e.target.textContent;
+            });
+        }
+    }
 });
+
+
+
+
+// Create an array. (The elements start at index 0.)
+// var ar = ["ab", "cd", "ef", "ab", "cd"];
+// Determine the first location of "cd".
+// document.write(ar.indexOf("ef") + "<br/>");
