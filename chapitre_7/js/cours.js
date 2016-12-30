39575222da145725a590aed8a33feecca415c6cb
ch.ntimba <ch.ntimba@bluewin.ch>
2016-12-31 00:31:56 +0100

    je viens de finir les animation css

--- chapitre_7/js/cours.js
+++ chapitre_7/js/cours.js
@@ -83,18 +83,25 @@ requestAnimationFrame(deplacerBloc); // Début de l'animation
 // Débuter une animation
 var cadre = document.getElementById('cadre');
 var bloc = document.getElementById('bloc');
-
-// Conversion en nombre du diamètre du bloc (valeur de la forme "XXpx")
-var diametreBloc = parseFloat(getComputedStyle(bloc).width);
 var vitesse = 7; // Valeur du déplacement en pixels
+// Conversion en nombre du diamètre du bloc (valeur de la forme "XXpx")
+var largeurBloc = parseFloat(getComputedStyle(bloc).width);
+var animationId = null; // Identifiant de l'animation
 
-// Déplace le bloc sur sa gauche
+// Déplace le bloc sur sa gauche jusqu'au bord du cadre
 function deplacerBloc() {
 	// Conversion en nombre de la position gauche du bloc (valeur de la forme "XXpx")
 	var xBloc = parseFloat(getComputedStyle(bloc).left);
-	// Déplacement du bloc
-	bloc.style.left = (xBloc + vitesse) + "px";
-	// Demande au navigateur d'appeler deplacerBloc dès que possible
-	requestAnimationFrame(deplacerBloc);
+	// Conversion en nombre de la largeur du cadre (valeur de la forme "XXpx")
+	var xMax = parseFloat(getComputedStyle(cadre).width);
+	if(xBloc + largeurBloc <= xMax) { // Si le bloc n'est pas encore au bout du cadre
+		// Déplacement du bloc
+		bloc.style.left = (xBloc + vitesse) + "px";
+		// Demande au naigateur d'appeler deplacerBloc dès que possible
+		animationId = requestAnimationFrame(deplacerBloc);
+	} else {
+		// Annulation de l'animation
+		cancelAnimationFrame(animationId);
+	}
 }
 requestAnimationFrame(deplacerBloc); // Début de l'animation
