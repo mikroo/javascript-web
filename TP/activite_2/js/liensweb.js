7732b502ffe714c38820d0e169d16ce97c1f9e76
ch.ntimba <ch.ntimba@bluewin.ch>
2017-01-01 02:03:16 +0100

    Debut du tp 2

---
+++ TP/activite_2/js/liensweb.js
@@ -0,0 +1,57 @@
+/* 
+Activité 1
+*/
+
+// Liste des liens Web à afficher. Un lien est défini par :
+// - son titre
+// - son URL
+// - son auteur (la personne qui l'a publié)
+var listeLiens = [
+    {
+        titre: "So Foot",
+        url: "http://sofoot.com",
+        auteur: "yann.usaille"
+    },
+    {
+        titre: "Guide d'autodéfense numérique",
+        url: "http://guide.boum.org",
+        auteur: "paulochon"
+    },
+    {
+        titre: "L'encyclopédie en ligne Wikipedia",
+        url: "http://Wikipedia.org",
+        auteur: "annie.zette"
+    }
+];
+
+// TODO : compléter ce fichier pour ajouter les liens à la page web
+
+for(var i = 0; i < listeLiens.length; i++) {
+    var articleElt = document.createElement('article');
+    articleElt.className = 'lien';
+
+    // Titre
+    var h4Elt = document.createElement('h4');
+    h4Elt.style.display = 'inline';
+    var aElt = document.createElement('a');
+        aElt.href = listeLiens[i].url;
+        aElt.textContent = listeLiens[i].titre;
+        aElt.style.textDecoration = 'none';
+        aElt.style.color = '#428bca';
+    h4Elt.appendChild(aElt);
+    articleElt.appendChild(h4Elt);
+
+   // Url
+   var spanElt = document.createElement('span');
+   spanElt.textContent = ' ' + listeLiens[i].url;
+   articleElt.appendChild(spanElt);
+
+   // Auteur
+   var auteurSpanElt = document.createElement('span');
+   auteurSpanElt.style.display = 'block';
+   auteurSpanElt.textContent = 'Ajouté par ' + listeLiens[i].auteur; 
+   
+   articleElt.appendChild(auteurSpanElt);
+
+   document.getElementById('contenu').appendChild(articleElt);
+}
