5dd2ead86424f6723aaa65f2859fbc01cb4ee14f
ch.ntimba <ch.ntimba@bluewin.ch>
2017-01-03 21:26:28 +0100

    Je viens de créer L'objet Form

--- TP/activite_2/js/liensweb.js
+++ TP/activite_2/js/liensweb.js
@@ -1,5 +1,7 @@
 /* 
 Activité 1
+Author : mikroo
+url : mikroo.com
 */
 
 // Liste des liens Web à afficher. Un lien est défini par :
@@ -25,10 +27,51 @@ var listeLiens = [
 ];
 
 // TODO : compléter ce fichier pour ajouter les liens à la page web
-/* Création du formulaire */
+/* Inclure une page externe */
+
+/*
+var Films = {
+  init: function (titre, annee, realisateur) {
+    this.titre = titre;
+    this.annee = annee;
+    this.realisateur = realisateur;
+  },
+  decrire: function(){
+    var description = this.titre + " (" + this.annee + ", " + this.realisateur + ")";
+    return description;
+  }
+
+}
+var film1 = Object.create(Films);
+film1.init("Le loup de wall Stree", 2013, "Martin Scorsese");
+*/
+
+
+
+// creer un formulaire avec l'objet form
+
+
 var ajoutLien = document.getElementById('ajoutLien');
 ajoutLien.addEventListener('click', function(){
-  // Enlever le bouton ajouter un lien
+  document.getElementById('formulaire').innerHTML = '';
+  // Début du formulaire
+  // creer un formularie
+  // creer le formulaire avec l'objet
+  var formElt = Form.createFormElt('form');
+  var nomElt = Form.createFormElt('input', 'Ton prénom'); 
+  var titreElt = Form.createFormElt('input', 'Titre du lien'); 
+  var lienElt = Form.createFormElt('input', 'url'); 
+  var sendElt = Form.createFormElt('button','Envoyer');
+
+  var tabElts = [nomElt, titreElt, lienElt, sendElt];
+
+  Form.createForm(formElt,tabElts,'formulaire');
+});
+
+/*
+//  Création du formulaire 
+ajoutLien.addEventListener('click', function(){
+  // supprime le bouton ajouter un lien
   document.getElementById('formulaire').innerHTML = '';
 
   var formElt = document.createElement('form');
@@ -41,6 +84,8 @@ ajoutLien.addEventListener('click', function(){
       lienElt.required = 'required';
   var buttonElt = document.createElement('button');
       buttonElt.textContent = "Ajouter";
+      buttonElt.type = "submit";
+
 
   // Ajout du formulaire à la page
   formElt.appendChild(nomElt);
@@ -48,44 +93,23 @@ ajoutLien.addEventListener('click', function(){
   formElt.appendChild(lienElt);
   formElt.appendChild(buttonElt);
   document.getElementById('formulaire').appendChild(formElt);
-
-  /* Gerer le formulaire */
-  formElt.addEventListener('submit', function(e){
-    var nom = nomElt.textContent;
-    console.log(nom);
-    e.preventDefault();
-  });
-
 });
 
-//1. Si le lien ne commence pas par http ou https On l'ajoute
+*/
 
-for(var i = 0; i < listeLiens.length; i++) {
-    var articleElt = document.createElement('article');
-    articleElt.className = 'lien';
 
-    // Titre
-    var h4Elt = document.createElement('h4');
-    h4Elt.style.display = 'inline';
-    var aElt = document.createElement('a');
-        aElt.href = listeLiens[i].url;
-        aElt.textContent = listeLiens[i].titre;
-        aElt.style.textDecoration = 'none';
-        aElt.style.color = '#428bca';
-    h4Elt.appendChild(aElt);
-    articleElt.appendChild(h4Elt);
+/* Gerer le formulaire qui va ajouter un lien */
+/*
+listeLiens.forEach(function(lien){
+  console.log(lien.url);
+})
 
-   // Url
-   var spanElt = document.createElement('span');
-   spanElt.textContent = ' ' + listeLiens[i].url;
-   articleElt.appendChild(spanElt);
+console.log('----------------------------');
+var autreLien = {'titre': 'mikroo inc', 'url' :'http://mikroo.com', 'auteur':'Chancy'};
+listeLiens.push(autreLien);
 
-   // Auteur
-   var auteurSpanElt = document.createElement('span');
-   auteurSpanElt.style.display = 'block';
-   auteurSpanElt.textContent = 'Ajouté par ' + listeLiens[i].auteur; 
-   
-   articleElt.appendChild(auteurSpanElt);
-
-   document.getElementById('contenu').appendChild(articleElt);
-}
+listeLiens.forEach(function(lien){
+  console.log(lien);
+})
+// Ajouter une entrer dans un tableau
+*/
