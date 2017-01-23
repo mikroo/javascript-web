d213c39a27e4a762c04700f9df0f415b51a55508
ch.ntimba <ch.ntimba@bluewin.ch>
2017-01-24 00:58:22 +0100

    je viens de finir l'exercice du chapitre 10

--- chapitre_10/js/article.js
+++ chapitre_10/js/article.js
@@ -1,18 +1,35 @@
 // https://oc-jswebsrv.herokuapp.com/article
 var formElt = document.querySelector('form');
-
 formElt.addEventListener('submit', function(e){
 	e.preventDefault();
+	var article = new FormData(formElt);
+	ajaxPost("http://oc-jswebsrv.herokuapp.com/articles", article, function(reponse){
+		// afficher un message au client 
+		var messageElt = document.createElement('p');
+		message.textContent = " L'article a bien été ajouté";
+		document.getElementById("resultat").appendChild(messageElt);
 
+		// console.log("L'article " + JSON.stringify(article) + " a été envoyé au serveur");
+	}, true );
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*
 	var article = {
 		titre: formElt.elements.titre.value,
 		contenu: formElt.elements.contenu.value
 	}
-
-	// Envoi de l'objet au serveur
-	ajaxPost("https://oc-jswebsrv.herokuapp.com/articles", article, function(){
-		// afficher un message au client 
-		console.log("L'article " + JSON.stringify(article) + " a été envoyé au serveur");
-	}, true );
-
-});
+	*/
